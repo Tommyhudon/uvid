664a9cf1c102fdc10e8d78ea9db6312d3975ef41
Tommy <tommy.hudon.1@ulaval.ca>
2020-04-29 15:11:09 -0400

    we the best

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -45,14 +45,23 @@ export default {
     joinRoom: async function (){
       this.$store.commit('setRoomId', this.roomCode);
       this.$store.commit('setUsername', this.name);
-      const roomRef = fb.firestore.collection('rooms').doc(`${this.roomCode}`);
+      const room = fb.db.ref('rooms/'+ this.roomCode);
+      room.once('value').then((snapshot) => {
+        const roomData = snapshot.val();
+        if(roomData == null){
+          console.log('room doesnt exist')
+        }else {
+          this.$router.push('Call');
+        }
+      })
+      /* const roomRef = fb.firestore.collection('rooms').doc(`${this.roomCode}`);
       const roomSnapshot = await roomRef.get();
       console.log('Got room:', roomSnapshot.exists);
       if (!roomSnapshot.exists) {
         console.log('room doesnt exist')
       } else {
         await this.$router.push('Call');
-      }
+      }*/
     },
   }
 }
--- src/components/Room.vue
+++ src/components/Room.vue
@@ -13,8 +13,10 @@ export default {
   data : function() {
     return{
       peerConnection : null,
+      remotePeerConnection: null,
       localStream: null,
-      remoteStream: null
+      remoteStream: null,
+      participant: 0
     }
   },
   computed: {
@@ -22,179 +24,117 @@ export default {
       return this.$store.state.roomId
     },
     configuration(){
-      return this.$store.state.configuration;
+      return this.$store.state.configuration
     },
     host() {
       return this.$store.state.host
+    },
+    name(){
+      return this.$store.state.username
     }
   },
   created: async function() {
-    await this.openUserMedia();
+    //await this.openUserMedia();
     if(this.host === true){
       await this.createRoom();
     }else {
-      console.log('Lets go')
       await this.joinRoom();
     }
   },
   methods: {
     createRoom: async function (){
-      const roomRef = await fb.firestore.collection('rooms').doc();
-
-      console.log('Create PeerConnection with configuration ', this.configuration);
-      this.peerConnection = new RTCPeerConnection(this.configuration);
-
-      this.localStream.getTracks().forEach(track => {
-        this.peerConnection.addTrack(track, this.localStream);
-      });
-
-      //Code for collection ICE candidate
-      const callerCandidatesCollection = roomRef.collection('callerCandidates');
-
-      this.peerConnection.addEventListener('icecandidate', event => {
-        if(!event.candidate){
-          console.log('Got final candidate!');
-          return;
-        }
-        console.log('Got candidate', event.candidate);
-        callerCandidatesCollection.add(event.candidate.toJSON());
-      });
-
-      //Code for creating a room
-      const offer = await this.peerConnection.createOffer();
-      await this.peerConnection.setLocalDescription(offer);
-      console.log('Created offer', offer);
-
-      const roomWithOffer = {
-        'offer' : {
-          type: offer.type,
-          sdp: offer.sdp
-        }
-      };
-
-      await roomRef.set(roomWithOffer);
-      this.$store.commit('setRoomId', roomRef.id);
-      console.log(`New room created with SDP offer. Room ID: ${roomRef.id}`);
-
-
-      this.peerConnection.addEventListener('track', event => {
-        console.log('Got remote track', event.streams[0]);
-        event.streams[0].getTracks().forEach(track => {
-          console.log('Add a track to the remoteStream', track);
-          this.remoteStream.addTrack(track);
-        })
-      });
-
-      //Listening for remote session
-      roomRef.onSnapshot(async snapshot => {
-        const data = snapshot.data();
-        if(!this.peerConnection.currentRemoteDescription && data && data.answer) {
-          console.log('Got remote description', data.answer);
-          const rtcSessionDescription = new RTCSessionDescription(data.answer);
-          await this.peerConnection.setRemoteDescription(rtcSessionDescription);
-        }
-      });
-
-      //listen for remote ICE candidates below
-      roomRef.collection('calleeCandidates').onSnapshot((snapshot => {
-        snapshot.docChanges().forEach(async change => {
-          if(change.type === 'added') {
-            let data = change.doc.data();
-            console.log(`Got new remote ICE candidate: ${JSON.stringify(data)}`);
-            await this.peerConnection.addIceCandidate(new RTCIceCandidate(data))
-          }
-        })
-      }))
+      //const roomRef = await fb.firestore.collection('rooms').doc();
+      const roomCode = '4444';
+      this.$store.commit('setRoomId', roomCode);
+      const room = fb.db.ref('rooms/' + roomCode + '/host');
+      await room.set({username: this.name});
+      console.log("create room : ", roomCode);
+      this.joinRoom()
     },
     joinRoom: async function (){
       console.log('Join room: ', this.code);
-      const roomRef = fb.firestore.collection('rooms').doc(`${this.code}`);
-      const roomSnapshot = await roomRef.get();
+      const roomRef = fb.db.ref('rooms/'+ this.code + '/Callers');
 
-      console.log('Create PeerConnection with configuration: ', this.configuration);
-      this.peerConnection = new RTCPeerConnection(this.configuration);
-
-      this.localStream.getTracks().forEach(track => {
-        this.peerConnection.addTrack(track, this.localStream);
-      });
-
-      // Code for collecting ICE candidates below
-      const calleeCandidatesCollection = roomRef.collection('calleeCandidates');
-      this.peerConnection.addEventListener('icecandidate', event => {
-        if (!event.candidate) {
-          console.log('Got final candidate!');
-          return;
-        }
-        console.log('Got candidate: ', event.candidate);
-        calleeCandidatesCollection.add(event.candidate.toJSON());
-      });
-
-      this.peerConnection.addEventListener('track', event => {
-        console.log('Got remote track:', event.streams[0]);
-        event.streams[0].getTracks().forEach(track => {
-          console.log('Add a track to the remoteStream:', track);
-          this.remoteStream.addTrack(track);
-        });
-      });
-
-      // Code for creating SDP answer below
-      const offer = roomSnapshot.data().offer;
-      console.log('Got offer:', offer);
-      await this.peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
-      const answer = await this.peerConnection.createAnswer();
-      console.log('Created answer:', answer);
-      await this.peerConnection.setLocalDescription(answer);
-
-      const roomWithAnswer = {
-        answer: {
-          type: answer.type,
-          sdp: answer.sdp,
-        },
-      };
-      await roomRef.update(roomWithAnswer);
-
-      // Listening for remote ICE candidates below
-      roomRef.collection('callerCandidates').onSnapshot(snapshot => {
-        snapshot.docChanges().forEach(async change => {
-          if (change.type === 'added') {
-            let data = change.doc.data();
-            console.log(`Got new remote ICE candidate: ${JSON.stringify(data)}`);
-            await this.peerConnection.addIceCandidate(new RTCIceCandidate(data));
-          }
-        });
-      });
-    },
-    openUserMedia: async function() {
-      this.localStream = await navigator.mediaDevices.getUserMedia(
+      const localStream =  await navigator.mediaDevices.getUserMedia(
         {video: true, audio: true});
-      this.remoteStream = new MediaStream();
-      console.log(this.remoteStream);
-      console.log(this.localStream);
 
+      //add video
       const user = document.createElement('video');
-      const remoteUser = document.createElement('video');
+      const textUser = document.createElement('div');
+      textUser.className = "overlay-name";
+      textUser.innerText = this.name;
 
-      remoteUser.srcObject = this.remoteStream;
-      user.srcObject = this.localStream;
-
+      user.srcObject = localStream;
       const room = document.getElementById('room');
       room.appendChild(user);
-      room.appendChild(remoteUser);
-
+      room.appendChild(textUser)
       user.play();
       user.muted = true;
-      remoteUser.play();
 
-    }
+      const localPeer = new SimplePeer({initiator: true, stream: localStream});
+
+
+      //Send signal
+      localPeer.on('signal', signalData => {
+        console.log('LocalPeer signal', signalData);
+        const callerRef = roomRef.child(this.name);
+        const newSignalDataRef = callerRef.push();
+        newSignalDataRef.set({
+          data:JSON.stringify(signalData)
+        })
+      });
+
+      //Listen to connection
+      roomRef.on('child_added', ({key: callerName}) => {
+        if(callerName !== this.name){
+          console.log("new child", callerName);
+          const peer = new SimplePeer();
+
+          //Listen to other caller's signal
+          const callerRef = roomRef.child(callerName);
+          callerRef.on('child_added', (res) => {
+            console.log('Caller doing something', res);
+            peer.signal(JSON.parse(res.val().data))
+          });
+
+          peer.on('stream', (stream) => {
+										  if(stream === undefined){
+										    console.log('no stream')
+            }else{
+										    console.log('yes stream');
+
+              const remoteCallerName = document.createElement('div');
+              remoteCallerName.className = "overlay-name";
+              remoteCallerName.innerText = callerName;
+
+              const newCaller = document.createElement('video');
+              newCaller.srcObject = localStream;
+              const room = document.getElementById('room');
+              room.appendChild(newCaller);
+              room.appendChild(remoteCallerName)
+              newCaller.play();
+            }
+          })
+        }
+      })
+    },
   }
 }
 </script>
-<style>
+<style lang="scss">
 	#room {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		background-color: #110022;
 		min-height: 100vh;
+			video {
+				z-index: 0;
+				height: 300px;
+				width: 300px;
+			}
+			.overlay-name{
+				z-index: 1
+			}
 	}
 </style>
